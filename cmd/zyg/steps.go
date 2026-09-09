299f0925a868a7a25ecb1fbf4aa2132bf30e327b
narayan-mindfire <narayanp@mindfiresolutions.com>
2026-09-09 13:15:00 +0530

    feat: add steps command
    Signed-off-by: narayan-mindfire <narayanp@mindfiresolutions.com>

---
+++ cmd/zyg/steps.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mindfire/zygote/trace"
+	"github.com/mindfire/zygote/vfs"
+	"github.com/spf13/cobra"
+)
+
+var stepsCmd = &cobra.Command{
+	Use:   "steps [run.zip]",
+	Short: "List steps in a recording",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		bundlePath := args[0]
+		store := vfs.NewMemStore()
+
+		bundle, err := trace.ImportFile(bundlePath, store)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(2) // bundle invalid
+		}
+
+		fmt.Printf("%-5s %-20s %-12s %s\n", "STEP", "NAME", "HASH", "EFFECTS")
+		for _, s := range bundle.Recording.Steps {
+			hashStr := s.Root
+			if len(hashStr) > 8 {
+				hashStr = hashStr[:8]
+			}
+			fmt.Printf("%-5d %-20s %-12s %d\n", s.N, s.Name, hashStr, s.Effects)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stepsCmd)
+}
